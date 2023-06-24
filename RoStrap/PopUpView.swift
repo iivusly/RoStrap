012806fde7bf9a3e4cbf59accf4f46c8de3a16a0
iivusly <iivusly@icloud.com>
2023-06-24 01:15:51 -0700

    Add PopUpView
    Signed-off-by: iivusly <iivusly@icloud.com>

---
+++ RoStrap/PopUpView.swift
@@ -0,0 +1,20 @@
+//
+//  PopUpView.swift
+//  RoStrap
+//
+//  Created by iivusly on 6/24/23.
+//
+
+import SwiftUI
+
+struct PopUpView: View {
+    var body: some View {
+		Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).padding()
+    }
+}
+
+struct PopUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        PopUpView()
+    }
+}
